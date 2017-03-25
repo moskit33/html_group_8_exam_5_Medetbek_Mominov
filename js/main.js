25c4d02edee4bfe1aeb6c7a0ad53d354e0aa81db
moskit <moskit544@gmail.com>
2017-03-25 20:01:21 +0600

    add footer and sign-block

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(".slider").slick({
 
 });
+
+$(document).ready(function(){
+    $(".navigation_header_menu a").on("click", function(event) {
+        event.preventDefault();
+        var id  = $(this).attr('href');
+
+        var top = $(id).offset().top;
+
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
