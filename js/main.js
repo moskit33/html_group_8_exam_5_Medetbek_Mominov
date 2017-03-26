cd0618805bad25c21e6aa6f37947cb55635a1674
moskit <moskit544@gmail.com>
2017-03-26 15:20:51 +0600

    fix header

--- js/main.js
+++ js/main.js
@@ -12,3 +12,19 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1000);
     });
 });
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+});
