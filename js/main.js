7c4094084bd5eb68c391cd713b62bba70826f2bd
moskit <moskit544@gmail.com>
2017-04-26 20:06:53 +0600

    fix icon

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-$(".slider").slick({
-
-});
-
 $(document).ready(function(){
+    $(".slider").slick({
+    });
     $(".navigation_header_menu a").on("click", function(event) {
         event.preventDefault();
         var id  = $(this).attr('href');
@@ -10,10 +8,7 @@ $(document).ready(function(){
         var top = $(id).offset().top;
 
         $('body,html').animate({scrollTop: top}, 1000);
-    });
-});
-$(document).ready(function(){
-
+        
     $(window).scroll(function(){
         if ($(this).scrollTop() > 100) {
             $('.scrollup').fadeIn();
